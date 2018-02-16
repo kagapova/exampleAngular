06ec398cf1904486f95a8327c4f0a8e39e8aaa00
Ivan Yurin <ivan-yurin@users.noreply.github.com>
2018-02-16 15:21:39 +0300

    Add Optimizely (#9)

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterContentInit} from '@angular/core';
 import {SearchService} from '../../services/search.service';
 import {Title} from '@angular/platform-browser';
+
+declare let window: any;
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterContentInit {
 
     constructor(private searchService: SearchService,
                 private titleService: Title) {
@@ -18,4 +20,7 @@ export class HomeComponent implements OnInit {
         this.titleService.setTitle('Decenter Search');
     }
 
+    ngAfterContentInit() {
+        window.optimizely.push(['activate', 10308853736]);
+    }
 }

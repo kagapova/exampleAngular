0759522f09542356cda861cc4aea384212491944
Ivan Yurin <yurin.ivan@gmail.com>
2018-03-06 17:39:47 +0300

    Fix

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,22 +6,9 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-    isRelative: boolean;
-
     constructor() {
     }
 
     ngOnInit() {
-        window.addEventListener('resize', this.onUpdateWindow, true);
-    }
-
-    onUpdateWindow() {
-        if (window.innerHeight <= window.screen.height) {
-            this.isRelative = true;
-        } else {
-            this.isRelative = false;
-        }
-
-        console.log("this.isRelative", this.isRelative)
     }
 }
